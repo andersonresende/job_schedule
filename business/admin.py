7e9e60d5eb36854712e92c440eb76b1af01772e2
Anderson Resende <andersonresende@MBP-de-Anderson.home>
2015-06-22 21:28:33 -0300

    Fixing bug category service choose

--- business/admin.py
+++ business/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django import forms
+
 from .models import (
     Service,
     Employee,
@@ -10,8 +12,28 @@ from .models import (
 )
 
 
+class ServiceForm(forms.ModelForm):
+    model = Service
+
+    def clean(self):
+        cleaned_data = super(ServiceForm, self).clean()
+        default_category_service = cleaned_data.get("default_category_service")
+        custom_category_service = cleaned_data.get("custom_category_service")
+
+        if not default_category_service and not custom_category_service:
+            self.add_error('default_category_service', "Select one category service.")
+            raise forms.ValidationError("Please, choose at least one Category Service.")
+
+
 class ServiceAdmin(admin.ModelAdmin):
     model = Service
+    form = ServiceForm
+    exclude = [
+        'creator',
+        'rule',
+        'end_recurring_period',
+        'calendar'
+    ]
 
 
 admin.site.register(Service, ServiceAdmin)
